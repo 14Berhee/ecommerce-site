b1dbc0729c7ef169b5bdbad7ed9ee05e392978ae
14Berhee <berhee14@gmail.com>
2025-01-07 12:48:57 +0800

    changes

--- pages/[category]/index.js
+++ pages/[category]/index.js
@@ -11,39 +11,40 @@ const ProductPage = () => {
     error,
     isLoading,
   } = useSWR(`https://fakestoreapi.com/products/category/${category}`, fetcher);
-  if (error) return <div>error</div>;
-  if (isLoading) return <div>Loading</div>;
 
-  const goToDetailPage = () => {
-    router.push("/product-detail");
+  if (error) return <div>Error</div>;
+  if (isLoading) return <div>Loading...</div>;
+
+  const goToDetailPage = (productId) => {
+    router.push(`/product-detail/${productId}`);
   };
 
   return (
     <div className="container mx-auto mt-10 max-w-[1290px]">
-      <div className="flex flex-wrap gap-5" oncClick={goToDetailPage}>
+      <div className="flex flex-wrap gap-5">
         {products?.map((product) => (
-          <div key={product.id}>
-            <div className="card bg-base-100 w-96 shadow-xl mt-4">
-              <figure>
-                <img
-                  className="max-w-[270px] h-[400px]"
-                  src={product.image}
-                  alt={product.title} // Provide a meaningful alt text
-                />
-              </figure>
-              <div className="card-body">
-                <h2 className="card-title">{product.title}</h2>
-                <p className="max-w-[250px]">{product.description}</p>
-                <div className="flex bg-slate-100 p-2">
-                  <p>{product.category}</p>
-                  <p>{product.price}$</p>
-                </div>
+          <div
+            key={product.id}
+            className="card bg-base-100 w-96 shadow-xl mt-4 cursor-pointer"
+            onClick={() => goToDetailPage(product.id)}
+          >
+            <figure>
+              <img
+                className="max-w-[270px] h-[400px]"
+                src={product.image}
+                alt={product.title} // Provide a meaningful alt text
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">{product.title}</h2>
+              <p className="max-w-[250px]">{product.description}</p>
+              <div className="flex bg-slate-100 p-2">
+                <p>{product.category}</p>
+                <p>{product.price}$</p>
               </div>
             </div>
           </div>
         ))}
-
-        {category}
       </div>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import useSWR from "swr";
 import Hero from "./Components/Hero";
-import { ProductDetail } from "./Components/ProductDetail";
+import ProductDetail from "./Components/product-detail/[id]";
 
 export default function Home() {
   return (
     <div>
       <div>
         <Hero />
+        <ProductDetail />
       </div>
     </div>
   );
